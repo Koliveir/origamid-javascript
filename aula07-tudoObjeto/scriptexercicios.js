f95e1de36b5e48ebca7fd9ba1f6cd32a89f92410
Erick Oliveira <erickoliveira.sp@gmail.com>
2026-04-22 10:16:15 -0300

    Exercícios da aula 07 foram feitos.

--- aula07-tudoObjeto/scriptexercicios.js
+++ aula07-tudoObjeto/scriptexercicios.js
@@ -1,6 +1,24 @@
 // nomeie 3 propriedades ou métodos de string
-
+var exemplo = "Texto";
+console.log(exemplo.repeat(55));
+console.log(exemplo.charAt(2));
+console.log(exemplo.length);
+console.log(exemplo.replace("x", ""));
 // nomeie 5 propriedades ou métodos de elementos do DOM
+var teste = document.querySelector("a");
+console.log(teste);
+document.querySelector(body);
+document.getElementById("#id");
+document.getElementsByClassName(".class");
+teste.innerHTML("Texto Inner");
+teste.setAttribute("color", "black");
 
 // busque na web um objeto (método) capaz de interagir com o clipboard
 // clipboard é a parte do seu navegador que lida com o CTRL + C e CTRL + V
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  alert(`Copiado o texto: ${copyText.value}`);
+}
